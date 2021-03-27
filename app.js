de7775f82516e1b9a854f9b97f77e4c7557cb8ff
unknown <harshitparikh2000@gmail.com>
2021-03-27 22:35:18 +0530

    new

---
+++ app.js
@@ -0,0 +1,31 @@
+// required libraries
+const express = require('express')
+const bodyParser = require('body-parser')
+const env = require('dotenv').config()
+const mongoose = require('mongoose')
+const array = [1]
+//configure app
+const app = express();
+
+//configuration app uses
+app.set('view engine','ejs')
+app.use(bodyParser.urlencoded({extended:true}))
+app.use(express.static("static"))
+
+// routes
+app.get('/',(req,res) => {
+    res.render('index',{title: 'Blooger'})
+})
+app.get('/blogs',(req,res) => {
+    res.render('blog',{title: 'Blogs'})
+})
+app.get('/about',(req,res) => {
+    res.render('about',{title: 'About'})
+})
+app.get('/contact',(req,res) => {
+    res.render('contact',{title: 'Contact'})
+})
+
+app.listen(process.env.PORT,() => {
+    console.log(`app is running on http://localhost:${process.env.PORT}`)
+})
